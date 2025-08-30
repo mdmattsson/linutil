e94b0c9cc2a685afbd9621d6e4a1773f6934322c
Michael Mattsson <michael@batlogic.com>
2025-08-30 15:59:57 -0400

    point to my mdm repo

--- core/tabs/system-setup/arch/omarchy.sh
+++ core/tabs/system-setup/arch/omarchy.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
-ansi_art='                 ▄▄▄                                                   
- ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄ 
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   █▀   ███   ███  ███   ███
-███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄ ███▄▄▄███
-███   ███  ███   ███   ███ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███  ▀▀▀▀▀▀███
-███   ███  ███   ███   ███  ███   ███ ██████████  ███   █▄   ███   ███  ▄██   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
- ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀ 
-                                       ███   █▀                                  '
+ansi_art='
+\033[37m                     .ed"""" """$$$$be.\033[0m
+\033[37m                   -"           ^""**$$$e.\033[0m
+\033[37m                 ."                   '\''$$$c\033[0m
+\033[37m                /                      "4$$b\033[0m
+\033[37m               d  3                      $$$$\033[0m
+\033[37m               $  *                   .$$$$$$\033[0m
+\033[37m              .$  ^c           $$$$$e$$$$$$$$.\033[0m
+\033[37m              d$L  4.         4$$$$$$$$$$$$$$b\033[0m
+\033[37m              $$$$b ^ceeeee.  4$$ECL.F*$$$$$$$\033[0m
+\033[37m  e$""=.      $$$$P d$$$$F $ $$$$$$$$$- $$$$$$\033[0m
+\033[37m z$$b. ^c     3$$$F "$$$$b   $"$$$$$$$  $$$$*"      .=""$c\033[0m
+\033[37m4$$$$L        $$P"  "$$b   .$ $$$$$...e$$        .=  e$$$.\033[0m
+\033[37m^*$$$$$c  %..   *c    ..    $$ 3$$$$$$$$$$eF     zP  d$$$$$\033[0m
+\033[37m  "**$$$ec   "   %ce""    $$$  $$$$$$$$$$*    .r" =$$$$P""\033[0m
+\033[37m        "*$b.  "c  *$e.    *** d$$$$$"L$$    .d"  e$$***"\033[0m
+\033[37m          ^*$$c ^$c $$$      4J$$$$$% $$$ .e*".eeP"\033[0m
+\033[37m             "$$$$$$"'\''$=e....$*$$**$cz$$" "..d$*"\033[0m
+\033[37m               "*$$$  *=%4.$ L L$ P3$$$F $$$P"\033[0m
+\033[37m                  "$   "%*ebJLzb$e$$$$$b $P"\033[0m
+\033[37m                    %..      4$$$$$$$$$$ "\033[0m
+\033[37m                     $$$e   z$$$$$$$$$$%\033[0m
+\033[37m                      "*$c  "$$$$$$$P"\033[0m
+\033[37m                       ."""*$$$$$$$$bc\033[0m
+\033[37m                    .-"    .$***$$$"""*e.\033[0m
+\033[37m                 .-"    .e$"     "*$c  ^*b.\033[0m
+\033[37m          .=*""""    .e$*"          "*bc  "*$e..\033[0m
+\033[37m        .$"        .z*"               ^*$e.   "*****e.\033[0m
+\033[37m        $$ee$c   .d"                     "*$.        3.\033[0m
+\033[37m        ^*$E")$..$"                         *   .ee==d%\033[0m
+\033[37m           $.d$$$*                           *  J$$$e*\033[0m
+\033[37m            """""                              "$$$"\033[0m
+'
 
 clear
 echo -e "\n$ansi_art\n"
@@ -18,15 +41,15 @@ sudo pacman -Sy --noconfirm --needed git
 
 # Use custom repo if specified, otherwise default to basecamp/omarchy
 OMARCHY_REPO="${OMARCHY_REPO:-basecamp/omarchy}"
-TITUS_REPO="${TITUS_REPO:-christitustech/omarchy-titus}"
+TITUS_REPO="${TITUS_REPO:-mdmattsson/omarchy-mattsson}"
 
 echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
 rm -rf ~/.local/share/omarchy/
 git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
 
-echo -e "\nCloning Titus-Omarchy from: https://github.com/${TITUS_REPO}.git"
-rm -rf ~/.local/share/omarchy-titus/
-git clone "https://github.com/${TITUS_REPO}.git" ~/.local/share/omarchy-titus >/dev/null
+echo -e "\nCloning Mattsson-Omarchy from: https://github.com/${TITUS_REPO}.git"
+rm -rf ~/.local/share/omarchy-mattsson/
+git clone "https://github.com/${TITUS_REPO}.git" ~/.local/share/omarchy-mattsson >/dev/null
 
 # Use custom branch if instructed
 if [[ -n "$OMARCHY_REF" ]]; then
@@ -37,4 +60,4 @@ if [[ -n "$OMARCHY_REF" ]]; then
 fi
 
 echo -e "\nInstallation starting..."
-source ~/.local/share/omarchy-titus/install.sh
+source ~/.local/share/omarchy-mattsson/install.sh
